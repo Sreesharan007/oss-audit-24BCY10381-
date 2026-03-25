600a7f2ac9043a92d0f78427fe04d9b4ab792a46
Sreesharan007 <sreesharan.p.n26@gmail.com>
2026-03-26 01:02:46 +0530

    Refactor disk auditor script for improved readability

--- script3_disk_auditor.sh
+++ script3_disk_auditor.sh
@@ -4,14 +4,25 @@
 DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
 
 echo "Directory Audit Report"
-echo "----------------------"
+echo "--------------------------------"
 
 for DIR in "${DIRS[@]}"; do
- if [ -d "$DIR" ]; then
-   PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
-   SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
-   echo "$DIR => Permissions: $PERMS | Size: $SIZE"
- else
-   echo "$DIR does not exist"
- fi
+    if [ -d "$DIR" ]; then
+        PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
+        SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
+        echo "$DIR => Permissions: $PERMS | Size: $SIZE"
+    else
+        echo "$DIR does not exist"
+    fi
 done
+
+echo ""
+echo "Checking Scheduling-related Directory (/proc)"
+CONFIG_DIR="/proc"
+
+if [ -d "$CONFIG_DIR" ]; then
+    PERMS=$(ls -ld $CONFIG_DIR | awk '{print $1, $3, $4}')
+    echo "$CONFIG_DIR => Permissions: $PERMS"
+else
+    echo "$CONFIG_DIR not found"
+fi
